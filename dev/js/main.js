e012cd370ddc09b994b280d564360f4f4660fa5a
Eric Njanga <eric.njanga@gmail.com>
2024-06-21 14:48:01 -0400

    (markup + styling) global - 2

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,13 +1,34 @@
 'use strict';
 
 import { handleViewportChange as handleViewportChange } from './videoBannerAccessibility.js';
+// import { videoBackgroundSetup as videoBackgroundSetup } from './video-background.js';
 
 document.addEventListener('DOMContentLoaded', function () {
   const mediaQuery = window.matchMedia('(min-width: 992px)');
 
   // Initial check
   handleViewportChange(mediaQuery);
+  // videoBackgroundSetup();
 
   // Listen for changes in the viewport width
   mediaQuery.addListener(handleViewportChange);
+});
+
+
+
+
+
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+  const stickyDiv = document.getElementById('main-nav');
+  const offsetTop = stickyDiv.offsetTop;
+
+  window.addEventListener('scroll', function() {
+      if (window.pageYOffset >= offsetTop) {
+          stickyDiv.classList.add('is-fixed');
+      } else {
+          stickyDiv.classList.remove('is-fixed');
+      }
+  });
 });
---
+++ dev/js/video-background.js
@@ -0,0 +1,12 @@
+
+
+
+export function videoBackgroundSetup() {
+    
+    // $('.video-background').backgroundVideo();
+
+
+    // $('.video-background').vide({
+    //     mp4: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+    // });
+}
